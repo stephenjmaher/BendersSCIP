cd9032e8a56863d640daae1303654560bb33fc20
Stephen J. Maher <s.maher3@lancaster.ac.uk>
2018-07-30 15:51:27 +0100

    fixes the helper script

--- sync_with_scip.sh
+++ sync_with_scip.sh
@@ -3,22 +3,22 @@
 # this is a simple script to sync all listed Benders' decomposition files with
 # the BendersSCIP repository.
 #
-# By default, the files will be copied from the input SCIP directory to the
-# BendersSCIP directory. To copy the files from the BendersSCIP directory to the
-# SCIP directory, then the -u parameter must be set
+# By default, the files will be copied from the BendersSCIP directory to the
+# the input SCIP directory. To copy the files from the input SCIP directory to
+# the BendersSCIP directory, then the -u parameter must be set
 
-UPDATE=0
+UPDATE=1
 SCIPDIR=""
 BENDERSFILES=""
 
-TOORFROM="from"
+TOORFROM="to"
 
 # parsing the command line arguments
 for i in $@
 do
    if test "$i" = "-u"
    then
-      UPDATE=1
+      UPDATE=0
    else
       if test -z $SCIPDIR
       then
@@ -32,25 +32,31 @@ done
 if test -z $BENDERSFILES
 then
    echo "All inputs have not been defined. Please specify the SCIP directory and the Benders' decomposition framework source files"
-   echo "Correct usage: sync_with_scip.sh <dir/to/scip> <benders source files>"
+   echo "Correct usage: sync_with_scip.sh <dir/to/scip> benders_files"
    exit 1
 fi
 
 SCIPPATH=`realpath $SCIPDIR`
 
 # Checking whether the user is sure they want to update the SCIP directory
+if [ "$UPDATE" = 0 ]
+then
+   echo "This action will update the BendersSCIP code with that from the SCIP directory"
+else
+   echo "This action will update the SCIP directory with the BendersSCIP code."
+fi
+
+read -p "Are you sure you want to do this? [y/N]: " yn
+
+# the default is No
+case $yn in
+   [Yy]* ) break;;
+   [Nn]* ) exit;;
+   * ) exit;;
+esac
+
 if [ "$UPDATE" = 1 ]
 then
-   echo "This action will update the SCIP directory with the BendersSCIP code."
-   read -p "Are you sure you want to do this? [y/N]: " yn
-
-   # the default is No
-   case $yn in
-      [Yy]* ) break;;
-      [Nn]* ) exit;;
-      * ) exit;;
-   esac
-
    TOORFROM="to"
 fi
 
